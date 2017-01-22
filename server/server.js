9cad4c95a693eb5ad43bc788c9f6706cd33d5ad0
Dan Turco <dan.turco@aescentia.com>
2017-01-21 20:44:38 -0700

    added cors to node server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,11 @@ const app = express();
 const http = require('http');
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
